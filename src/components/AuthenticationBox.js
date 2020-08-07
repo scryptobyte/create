da9fe1fd00d5c84771e6d4a8543ff17919a97a1a
Shubham Kumar Rastogi <shubham.kr@ShubhamRastogi.local>
2020-08-08 01:35:00 +0530

    learning react

---
+++ src/components/AuthenticationBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AuthenticationBox.css'
+
+class AuthenticationBox extends React.Component{
+    render() {
+        return (
+            <div id = "authBox">
+                <Route path='/' component={HomaComponent}>
+            </div>
+        );
+    }
+}
+
+export default AuthenticationBox;
